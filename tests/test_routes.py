9f4776942e4cf8859cf46b73547bb359f3ff8de2
AZiemelis <aziemelis@proton.me>
2024-07-12 11:29:14 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,8 +22,9 @@ BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 ######################################################################
-#  T E S T   C A S E S
+# T E S T   C A S E S
 ######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -40,6 +41,7 @@ class TestAccountService(TestCase):
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
+        pass
 
     def setUp(self):
         """Runs before each test"""
@@ -52,9 +54,9 @@ class TestAccountService(TestCase):
         """Runs once after each test case"""
         db.session.remove()
 
-    ######################################################################
-    #  H E L P E R   M E T H O D S
-    ######################################################################
+######################################################################
+# H E L P E R   M E T H O D S
+######################################################################
 
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
@@ -72,9 +74,9 @@ class TestAccountService(TestCase):
             accounts.append(account)
         return accounts
 
-    ######################################################################
-    #  A C C O U N T   T E S T   C A S E S
-    ######################################################################
+######################################################################
+# A C C O U N T   T E S T   C A S E S
+######################################################################
 
     def test_index(self):
         """It should get 200_OK from the Home Page"""
